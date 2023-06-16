77f328fd5bbc1c10f0b8bc37c574e63e125bc4dc
jahamben <joshhamblen10@gmail.com>
2023-06-16 13:45:39 -0600

    week

--- login.cmd
+++ login.cmd
@@ -1,6 +1,6 @@
 echo "Logging in"
 
-curl --insecure -v -d "@login.json" POST -H "Content-Type:application/json" http://localhost:3000/login
+curl --insecure -v -d "@login.json" POST -H "Content-Type:application/json" https://joshhamblen.cit270.com/login
 
 @REM curl -v https://dev.stedi.me/validate/95fa12c4-5e2b-412d-8509-3eb3596ae8e
 
