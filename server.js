eb64f066a5f10d178adee38fbed36213ab0f661d
jahamben <joshhamblen10@gmail.com>
2023-07-03 13:42:50 -0600

    redis

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const Redis = require('redis');
 const app=express();
 const port = 3000;
-const redisClient = Redis.createClient({url:'redis://127.0.0.1:6379'});
+const redisClient = Redis.createClient({url:'redis//:redis-stedi-josh:6379'});
 
 
 app.use(bodyParser.json()); //allow JSON (Javascript Object Notation) requests
